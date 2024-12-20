ab141190ffb117f74677838004d33eb3f20e3ea9
Yan-Xiangjun <xmascreater@163.com>
2024-12-20 16:31:52 +0800

    更新

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
-pip -m pip install -U -i https://mirrors.ustc.edu.cn/pypi/simple pip
+python -m pip install -U -i https://mirrors.ustc.edu.cn/pypi/simple pip
 pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple wheel
 pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple pyinstaller
-pyinstaller -F main.py 
+pyinstaller main.py 
+python -c "import shutil; shutil.make_archive('./mydist', 'zip', './dist/main')"
