5af11fb392426f8bf7723f8e1153ffa841a2de05
Yan-Xiangjun <xmascreater@163.com>
2024-12-30 23:20:03 +0800

    测试

--- build.py
+++ build.py
@@ -6,5 +6,5 @@ print('创建虚拟环境……')
 py_name = 'python3' if sys.platform == 'linux' else 'python'
 os.system(f'{py_name} -m pip install -U -i https://mirrors.ustc.edu.cn/pypi/simple pip')
 os.system('pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple wheel')
-os.system('pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple pyinstaller')
+os.system('pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple pyinstaller pynput')
 os.system('pyinstaller main.py')
