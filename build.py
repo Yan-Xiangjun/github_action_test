8ff1efeee10a6adad3a66931dc2cb0387111c5b0
Yan-Xiangjun <xmascreater@163.com>
2024-12-30 19:35:42 +0800

    提交

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+print('创建虚拟环境……')
+
 py_name = 'python3' if sys.platform == 'linux' else 'python'
 os.system(f'{py_name} -m pip install -U -i https://mirrors.ustc.edu.cn/pypi/simple pip')
 os.system('pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple wheel')
