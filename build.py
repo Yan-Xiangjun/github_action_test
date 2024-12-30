ce0f204cdd5ad528c6240605d265a1ceeb100980
Yan-Xiangjun <xmascreater@163.com>
2024-12-30 23:28:16 +0800

    测试

--- build.py
+++ build.py
@@ -7,4 +7,4 @@ py_name = 'python3' if sys.platform == 'linux' else 'python'
 os.system(f'{py_name} -m pip install -U -i https://mirrors.ustc.edu.cn/pypi/simple pip')
 os.system('pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple wheel')
 os.system('pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple pyinstaller pynput')
-os.system('pyinstaller main.py')
+os.system('pyinstaller main.py --hidden-import=pynput.keyboard._xorg --hidden-import=pynput.mouse._xorg')
