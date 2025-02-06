441ac718b6bd1143797b11f977e3769426a0647b
Yan-Xiangjun <xmascreater@163.com>
2025-02-06 20:37:50 +0800

    test

--- build.py
+++ build.py
@@ -6,5 +6,6 @@ print('packing')
 py_name = 'python3' if sys.platform == 'linux' else 'python'
 os.system(f'{py_name} -m pip install -U -i https://mirrors.ustc.edu.cn/pypi/simple pip')
 os.system('pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple wheel')
-os.system('pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple pyinstaller pynput')
-os.system('pyinstaller main.py --hidden-import=pynput.keyboard._xorg --hidden-import=pynput.mouse._xorg')
+os.system('pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple pyinstaller pynput torch')
+os.system(
+    'pyinstaller main.py --hidden-import=pynput.keyboard._xorg --hidden-import=pynput.mouse._xorg')
